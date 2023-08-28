4bf5eb4998f21e406a72191e09ad03aab18bb7dc
Siddharth Subramoni <siddharth.subramoni@gmail.com>
2023-08-27 22:25:47 -0400

    fix: failing tests

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 
-describe('Test Suite 1', ()=> {
-    test('fail test case', () => {
-        throw new Error('test suite failed...')
-    })
-})
+// describe('Test Suite 1', ()=> {
+//     test('fail test case', () => {
+//         throw new Error('test suite failed...')
+//     })
+// })
